1212ff39a6937ea92af015af0f967555b54db043
Ucheze Victor <uchezevictor6@gmail.com>
2026-08-28 00:41:36 +0100

    fix: update session handling to refresh auth token correctly

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -59,7 +59,7 @@ export async function updateSession(request: NextRequest) {
   );
 
   // refreshing the auth token
-  await supabase.auth.getUser();
+  await supabase.auth.getSession();
 
   return supabaseResponse;
 }
